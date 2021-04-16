7b3426f5e93c62dbfcc657dc32c91f53d66e8a31
Ben Keys <bkeys818@gmail.com>
2021-04-16 09:10:00 -0400

    :bento: Clean up svgs

--- src/assets/svgs.tsx
+++ src/assets/svgs.tsx
@@ -1,48 +1,65 @@
-import React from "react";
+import React, { useState } from "react"
 
 import "./svgs.scss"
 
-type CustomSVGProps = Omit<React.SVGProps<SVGSVGElement>, "xmlns" | "viewBox">
-function customSVGTemplate(viewBox: string, name: string, props: CustomSVGProps) {
-    const { className, ...otherProps} = props
-    return {
-        ...{
-            xmlns: "http://www.w3.org/2000/svg",
-            viewBox: viewBox,
-            className: `symbol_${name} ` + (className ?? "")
-        },
-        ...otherProps
-    } as React.SVGProps<SVGSVGElement>
+type SVGTemplateProps = Omit<React.SVGProps<SVGSVGElement>, "xmlns"> & {
+    name: string
+    viewBox: Required<React.SVGProps<SVGSVGElement>>["viewBox"]
 }
-
-export const ChevronRight = (props: CustomSVGProps) => 
-    <svg {...customSVGTemplate("0 0 14 14", "chevron-right", props)} >
-        <title>symbol_chevron-right</title>
-        <path d="m3,1l6,6l-6,6" fill="none" stroke="currentcolor" strokeWidth={props.strokeWidth ?? 2}/>
-    </svg>
-
-export class Selected extends React.Component<Omit<CustomSVGProps, "onClick">, {visible: boolean}> {
-    constructor(props: Omit<CustomSVGProps, "onClick">) {
-        super(props)
-        this.state = {
-            visible: false
-        }
-        this.handleClick = this.handleClick.bind(this);
-    }
-    handleClick() {
-        this.setState({ visible: !this.state.visible })
-    }
-    render() {
-        return <svg {...customSVGTemplate("-5 -5 10 10", "selected", this.props)} onClick={this.handleClick}>
-            <title>symbol_selected</title>
-            <circle r="4.5" fill="none" stroke="currentcolor" strokeWidth={this.props.strokeWidth ?? 1}/>
-            <circle r="2.75" visibility={this.state.visible ? "visible" : "hidden"}/>
+function SVGTemplate(props: SVGTemplateProps) {
+    let { name, className, children, ...otherProps } = props
+    name = "symbol_" + name
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className={name + (className ? ` ${className}` : "")}
+            {...otherProps}
+        >
+            <title>{name}</title>
+            {children}
         </svg>
-    }
+    )
 }
 
-export const Xmark = (props: CustomSVGProps) => 
-    <svg {...customSVGTemplate("0 0 14 14", "xmark", props)}>
-        <title>symbol_xmark</title>
-        <path d="m2,2 10,10m-10,0 10,-10"  fill="none" stroke="currentcolor" strokeWidth={props.strokeWidth ?? 2}/>
-    </svg>
+
+
+export const ChevronRight = () =>
+    <SVGTemplate name="chevron-right" viewBox="0 0 14 14">
+        <path d="m3,1l6,6l-6,6" strokeWidth="2" />
+    </SVGTemplate>
+
+
+interface SelectedProps {
+    active: boolean
+}
+export const Selected = (props: SelectedProps) =>
+    <SVGTemplate name="selected" viewBox="-5 -5 10 10">
+        <circle r="2.75" visibility={props.active ? "visible" : "hidden"} />
+        <circle r="4.5" fill="none" stroke="currentcolor"/>
+    </SVGTemplate>
+
+
+export const Xmark = () =>
+    <SVGTemplate name="xmark" viewBox="0 0 14 14">
+        <path d="m2,2 10,10m-10,0 10,-10" strokeWidth="2" />
+    </SVGTemplate>
+
+
+interface LoadStatusProps {
+    dataState: "DEFAULT" | "ACTIVE" | "SUCCESS" | "ERROR"
+}
+const loadStatusPaths: { [key in Required<LoadStatusProps>["dataState"]]: [string, string] } = {
+    DEFAULT: ["", ""],
+    ACTIVE: [
+        "M 60 60 m -45, 0 a 45,45 0 1,1 90,0 a 45,45 0 1,1 -90,0",
+        "M15,60a45,45,0,0,0,45,45",
+    ],
+    ERROR: ["m15,60 30,30", "m45 90 60,-60"],
+    SUCCESS: ["m20,20 80,80", "m20,100 80,-80"],
+}
+export const LoadStatus = (props: LoadStatusProps) =>
+    <SVGTemplate name="load-status" viewBox={"0, 0, 120, 120"}>
+        <path d={loadStatusPaths[props.dataState][0]} />
+        <path d={loadStatusPaths[props.dataState][1]} />
+    </SVGTemplate>
+

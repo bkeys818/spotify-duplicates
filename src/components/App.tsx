47df0935423ce4a0d84d657b7699b958ba2f1501
Ben Keys <bkeys818@gmail.com>
2021-04-30 20:31:11 -0400

    :building_construction: Create App component

---
+++ src/components/App.tsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react"
+
+import { request } from "../spotify"
+
+import Playlist from "./Playlist"
+
+
+interface AppProps {
+    accessToken: string
+}
+interface AppStates {
+    playlists: SimplifiedPlaylistObject[]
+    selected?: SimplifiedPlaylistObject | null
+}
+
+export default class App extends Component<AppProps, AppStates> {
+    constructor(props: AppProps) {
+        super(props)
+        this.state = {
+            playlists: [],
+        }
+    }
+
+    componentDidMount() {
+        request("Get a List of Current User's Playlists", {
+            token: this.props.accessToken ?? "",
+        })
+            .then(res => {
+                this.setState({ playlists: res?.items ?? this.state.playlists })
+            })
+            .catch(console.error)
+    }
+
+    render() {
+        const { playlists } = this.state
+        return (
+            <div className="app-wrapper">
+                <div className="playlist-container">
+                    {playlists.map(playlist => (
+                        <Playlist
+                            key={playlist.id}
+                            name={playlist.name}
+                            images={playlist.images}
+                            tracks={playlist.tracks}
+                        />
+                    ))}
+                </div>
+            </div>
+        )
+    }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,61 +1,29 @@
 import React, { Component } from "react"
 
+import App from "../components/App"
+
 import { navigate } from "gatsby"
 import Cookies from "universal-cookie"
 
-import Playlist from "../components/Playlist"
-import { request } from "../spotify/request"
-
-import "./index.scss"
-
-interface AppProps {
-    location: Location
-}
-interface AppStates {
-    accessToken?: string
-    playlists: SimplifiedPlaylistObject[]
-}
-
-export default class App extends Component<AppProps, AppStates> {
-    private readonly cookies = new Cookies()
-
-    constructor(props: AppProps) {
+interface IndexProps { location: Location }
+interface IndexStates { token?: string }
+export default class Index extends Component<IndexProps, IndexStates> {
+    constructor(props: IndexProps) {
         super(props)
-
-        let accessToken = authenticate(props.location.hash)
-
         this.state = {
-            accessToken: accessToken,
-            playlists: [],
+            token: authenticate(props.location.hash),
         }
     }
 
     componentDidMount() {
-        if (!this.state.accessToken) navigate("/login/")
+        if (!this.state.token) navigate("/login/")
         window.location.hash = ""
-        request("Get a List of Current User's Playlists", {
-            token: this.state.accessToken ?? "",
-        })
-            .then(res => {
-                this.setState({ playlists: res?.items ?? this.state.playlists })
-            })
-            .catch(console.error)
     }
 
     render() {
-        return React.createElement("div", {
-            className: "playlist-container"
-        },
-        ...this.state.playlists.map(playlist => (
-            <Playlist
-                key={playlist.id}
-                name={playlist.name}
-                images={playlist.images}
-                tracks={playlist.tracks}
-            />
-        ))
-        )
+        return <App accessToken={this.state.token!}/>
     }
+
 }
 
 function authenticate(hashStr: Location["hash"]) {
